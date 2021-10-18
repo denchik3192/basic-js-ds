1d2a3f242b2f673155ab65c145887aa15dade1c0
DENIS <den465341@gmail.com>
2021-10-18 23:23:20 +0300

    feat: implement binary search tree

--- src/st-queue.js
+++ src/st-queue.js
@@ -20,14 +20,12 @@ module.exports = class Queue {
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue( value ) {
+    
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
   }
 
 }
